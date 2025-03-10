432694b469bc5ca883809b6252a123aba4486b15
aarondobson-wonde <adobson@wonde.com>
2025-03-10 12:35:41 +0000

    Stop after credit overflow

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,27 @@
 function addNotes() {
   const values = document.querySelectorAll("td.right>input");
   const inputFields = document.querySelectorAll("div.field>span.formulaTextBoxHolder>input");
-  values.forEach((item, x) => {
-    inputFields[x].value = parseFloat(item.attributes.value.nodeValue);
-  });
+  const totalAllowedCredit = parseFloat(document.getElementById("AmountDue").textContent.replace(/,/g, ''));
+  let allocatedCredit = 0;
+
+  for (let x = 0; x < values.length; x++) {
+    let item = values[x];
+    let fieldAmount = parseFloat(item.attributes.value.nodeValue);
+    allocatedCredit += fieldAmount;
+
+    if (allocatedCredit <= totalAllowedCredit) {
+      inputFields[x].value = fieldAmount;
+    }
+    else {
+      fieldAmount -= allocatedCredit - totalAllowedCredit;
+      inputFields[x].value = fieldAmount;
+      break;
+    }
+  }
 }
 
 chrome.action.onClicked.addListener((tab) => {
-  if (!tab.url.includes('https://chrome')) {
+  if (tab.url.includes('https://go.xero.com/Credits/Allocate')) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: addNotes
